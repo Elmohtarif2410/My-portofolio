a42d39e96da4850b8418758216649dfcdd8e50f4
ahmed kamal <101001511+Elmohtarif2410@users.noreply.github.com>
2022-05-26 11:44:59 +0200

    V2.0.2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,82 @@
+/****************************************************************************
+********************* Functionalty of slide to landaing *********************
+****************************************************************************/
+let landing = document.querySelector(".landing"),
+    landingButton = document.querySelectorAll(".landing ul li");
+
+landingButton[0].onclick = function () {
+    //add and remove classes for buttons
+    landingButton[0].classList.add("active");
+    landingButton[1].classList.remove("active");
+    landingButton[2].classList.remove("active");
+    // change to image
+    landing.style.backgroundImage = "url(images/landing-1.jpg)";
+}
+
+landingButton[1].onclick = function () {
+    //add and remove classes for buttons
+    landingButton[1].classList.add("active");
+    landingButton[0].classList.remove("active");
+    landingButton[2].classList.remove("active");
+    // change to image
+    landing.style.backgroundImage = "url(images/landing-2.jpg)";
+}
+
+landingButton[2].onclick = function () {
+    //add and remove classes for buttons
+    landingButton[2].classList.add("active");
+    landingButton[0].classList.remove("active");
+    landingButton[1].classList.remove("active");
+    // change to image
+    landing.style.backgroundImage = "url(images/landing-3.jpg)";
+}
+
+/****************************************************************************
+********************* Functionalty of slide to Testmonials ******************
+****************************************************************************/
+let quote = document.querySelector(".testmonials blockquote"),
+    nameQuote = document.querySelector(".testmonials h3"),
+    testmonialsButton = document.querySelectorAll(".testmonials ul li");
+
+testmonialsButton[0].onclick = function () {
+    //add and remove classes for buttons
+    testmonialsButton[0].classList.add("active");
+    testmonialsButton[1].classList.remove("active");
+    testmonialsButton[2].classList.remove("active");
+    // change to image
+        quote.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem accusamus laborum deserunt, illo reprehenderit repellendus aliquam, dolorum unde mollitia veritatis odio. Exercitationem voluptatibus facilis error atque possimus perferendis.";
+        nameQuote.textContent = "name one";
+}
+
+testmonialsButton[1].onclick = function () {
+    //add and remove classes for buttons
+    testmonialsButton[1].classList.add("active");
+    testmonialsButton[0].classList.remove("active");
+    testmonialsButton[2].classList.remove("active");
+    // change to image
+    quote.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem accusamus laborum deserunt, illo reprehenderit repellendus aliquam, dolorum unde mollitia veritatis odio. Exercitationem voluptatibus facilis error atque possimus.";
+    nameQuote.textContent = "name Two";
+
+}
+
+testmonialsButton[2].onclick = function () {
+    //add and remove classes for buttons
+    testmonialsButton[2].classList.add("active");
+    testmonialsButton[0].classList.remove("active");
+    testmonialsButton[1].classList.remove("active");
+    // change to image
+    quote.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem accusamus laborum deserunt, illo reprehenderit repellendus aliquam, dolorum unde mollitia veritatis odio. Exercitationem voluptatibus facilis error atque.";
+    nameQuote.textContent = "name three"
+}
+
+/****************************************************************************
+************************** functionalty of Anmition *************************
+****************************************************************************/
 let anmation = document.querySelectorAll(".skills section.progress div span"),
     body = document.querySelector("body");
 
 window.onscroll = function () {
+    // function button scroll top page
     if (window.scrollY >= 700) {
         topPage.style.opacity = "1";
         topPage.style.display = "block";
@@ -10,7 +85,9 @@ window.onscroll = function () {
         topPage.style.display = "none";
     }
 
+    // Anmiation to Skills section
     if (body.scrollWidth > 1200) {
+        console.log("test")
         if (window.scrollY >= 1300) {
             anmation[0].style.animation = "progress ease-in-out 1.5s running";
             anmation[1].style.animation = "progress ease-in-out 1.5s running";
@@ -39,13 +116,22 @@ window.onscroll = function () {
     }
 };
 
+/****************************************************************************
+********************* Functionalty for onclick buttons **********************
+****************************************************************************/
 let icon = document.getElementById("icon"),
+    logo = document.querySelector("header .logo"),
     buttonScills = document.querySelector(".skills .container section.content button "),
     bottonLearnMore = document.querySelector(".landing .text button"),
-    bottonDownloadResume = document.querySelector(".resume button");
+    bottonDownloadResume = document.querySelector(".resume button"),
+    portfolioButtonMore = document.querySelector(".portfolio button");
 
 icon.onclick = function () {
     icon.classList.toggle("click");
+};
+
+logo.onclick = function () {
+    open("#home", "_self");
 };
 
 topPage.onclick = function () {
@@ -68,18 +154,66 @@ bottonDownloadResume.onclick = function () {
     open("https://www.linkedin.com/in/%D8%A3%D8%AD%D9%85%D8%AF-%D9%83%D9%85%D8%A7%D9%84-a458a41a2/");
 }
 
+portfolioButtonMore.onclick = function () {
+    if (this.textContent === "more") {
+        document.querySelector(".portfolio .projects").style.maxHeight = "unset";
+        this.textContent = "less";
+    } else {
+        document.querySelector(".portfolio .projects").style.maxHeight = "750px";
+        this.textContent = "more";
+    }
+}
 
+/****************************************************************************
+********************* functionalty Buttons Action Page **********************
+*************************** functionalty portflio ***************************
+****************************************************************************/
+// functionalty portflio category
+let category = document.querySelectorAll(".portfolio ul li"),
+    projectCategory = document.querySelectorAll(".portfolio .projects section .text span");
 
-var project_1 = document.querySelector("#project-1"),
-    project_2 = document.querySelector("#project-2"),
-    project_3 = document.querySelector("#project-3"),
-    project_4 = document.querySelector("#project-4"),
-    project_5 = document.querySelector("#project-5"),
-    project_6 = document.querySelector("#project-6"),
-    project_7 = document.querySelector("#project-7"),
-    project_8 = document.querySelector("#project-8"),
-    project_9 = document.querySelector("#project-9");    
+for (c = 1; c < category.length; c++) {
+    category[c].onclick = function () {
+        //remove class active of category
+        for (i = 0; i < category.length; i++) {
+            category[i].classList.remove("active");
+        }
+        // add class active for this category
+        this.classList.add("active");
 
+        // restet section projets
+        for (p = 0; p < projectCategory.length; p++) {
+            projectCategory[p].parentElement.parentElement.style.display = "block";
+        }
+    
+        // shfile projects
+        categoryName = this.textContent; // varible to content this category
+        // loop delete section projects when click this category
+        for (i = 0; i < projectCategory.length; i++) {
+            projectCategoryName = projectCategory[i].textContent;
+            if (categoryName != projectCategoryName) {
+                projectCategory[i].parentElement.parentElement.style.display = "none";
+            }
+        }
+    }
+}    
+// function to click All projects
+category[0].onclick = function () {
+    // restet section projets
+    for (p = 0; p < projectCategory.length; p++) {
+        projectCategory[p].parentElement.parentElement.style.display = "block";
+    }
+    // remove class active of category
+    for (i = 0; i < category.length; i++) {
+        category[i].classList.remove("active");
+    }
+    // add class active for this category
+    this.classList.add("active");
+}
+
+/****************************************************************************
+****************** Functionalty of open method to projects ******************
+****************************************************************************/
 project_1.onclick = function () {
     open("https://elmohtarif2410.github.io/My-portofolio/");
 };
@@ -106,4 +240,13 @@ project_8.onclick = function () {
 };
 project_9.onclick = function () {
     open("https://elmohtarif2410.github.io/order-summary-card/");
+};
+project_10.onclick = function () {
+    open("");
+};
+project_11.onclick = function () {
+    open("");
+};
+project_12.onclick = function () {
+    open("");
 };
